8f53dcbccc44a41b5d445b04b7bbeb104177bdfe
𝘔𝘢𝘵𝘩𝘦𝘶𝘴 𝘈𝘭𝘮𝘦𝘪𝘥𝘢 𝘙𝘪𝘨𝘶𝘦𝘪𝘳𝘢 <117239590+matheusalrg@users.noreply.github.com>
2026-03-17 23:48:45 -0300

    Update tarefa02.c

--- tarefas/tarefa02/tarefa02.c
+++ tarefas/tarefa02/tarefa02.c
@@ -1,1 +1,27 @@
+// Acessando o conteúdo apontado por um ponteiro - operador de indireção
 
+#include <stdio.h>
+#include <locale.h>
+
+int main() {
+    setlocale(LC_ALL, "Portuguese");
+    // Variável tesouro
+    int tesouro_local = 0;
+
+    // Criação do ponteiro da plataforma
+    int *plataforma_secreta;
+
+    // Conectando o ponteiro ao endereço do tesouro local
+    plataforma_secreta = &tesouro_local;
+
+    // Mensagem da situação do tesouro(0 - Para não encontrado; 1 - Para encontrado)
+    printf("O tesouro ainda não apareceu. Valor: %d\n", tesouro_local);
+
+    // Chamando o ponteiro para acessar e alterar o valor da variável tesouro local para 1
+    *plataforma_secreta = 1;
+
+    // Mensagem da situação do tesouro(0 - Para não encontrado; 1 - Para encontrado)
+    printf("O personagem pisou na plataforma secreta! O tesouro apareceu! Valor: %d\n", tesouro_local);
+
+    return 0;
+}
