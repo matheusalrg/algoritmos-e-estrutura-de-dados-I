fed7a721080ec163f1d8d0f3e4e2ad87ef61fac5
𝘔𝘢𝘵𝘩𝘦𝘶𝘴 𝘈𝘭𝘮𝘦𝘪𝘥𝘢 𝘙𝘪𝘨𝘶𝘦𝘪𝘳𝘢 <117239590+matheusalrg@users.noreply.github.com>
2026-03-18 01:10:23 -0300

    Update tarefa04.c

--- tarefas/tarefa04/tarefa04.c
+++ tarefas/tarefa04/tarefa04.c
@@ -1,1 +1,28 @@
+// Ponteiros como parâmetros de funções
 
+#include <stdio.h>
+#include <locale.h>
+
+// Função sem retorno que dobra a pontuação do jogador utilizando um ponteiro
+void aplicar_powerup_pontuacao_dupla ( int *pontuacao ) {
+    // Acessa o valor apontado pelo ponteiro e o multiplica por 2, alterando diretamente a variável original
+    *pontuacao = *pontuacao * 2;    
+}
+
+int main() {
+    setlocale(LC_ALL, "Portuguese");
+
+    // Variável que armazena a pontuação do jogador
+    int pontuacao_jogador = 1500;
+
+    // Exibe a pontuação do jogador antes do power-up
+    printf("Pontuacao antes do power-up: %d\n", pontuacao_jogador);
+
+    // Chama a função passando o endereço da variável para que ela seja modificada diretamente
+    aplicar_powerup_pontuacao_dupla(&pontuacao_jogador);
+
+    // Exibe a pontuação do jogador após o power-up
+    printf("Pontuacao apos o power-up: %d\n", pontuacao_jogador);
+
+    return 0;
+}
