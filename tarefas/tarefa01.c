32b4f7367e0a5d11f73d29cfcb8b76516c85cc11
𝘔𝘢𝘵𝘩𝘦𝘶𝘴 𝘈𝘭𝘮𝘦𝘪𝘥𝘢 𝘙𝘪𝘨𝘶𝘦𝘪𝘳𝘢 <117239590+matheusalrg@users.noreply.github.com>
2026-03-17 22:29:39 -0300

    Update tarefa01.c

--- tarefas/tarefa01.c
+++ tarefas/tarefa01.c
@@ -1,1 +1,24 @@
+/* Introdução aos ponteiros */
 
+# include <stdio.h>
+
+int main() {
+    /* Variável de vida */
+    int vida_jogador = 100;
+
+    /* Criação do ponteiro para a vida */
+    int *ponteiro_vida;
+
+    /* Simulador de dano */
+    vida_jogador = 50;
+    printf("O jogador sofreu dano! Vida atual: %d\n", vida_jogador);
+
+    /* Conectando o ponteiro ao endereço de vida do jogador */
+    ponteiro_vida = &vida_jogador;
+
+    /* Power-up (restaurando a vida via ponteiro) */
+    *ponteiro_vida = 100;
+    printf("Power-up coletado! Vida restaurada: %d\n", vida_jogador);
+
+    return 0;
+}
