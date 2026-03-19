66c8b43e34928fe9ed08c31706f2d51c6a7f2303
𝘔𝘢𝘵𝘩𝘦𝘶𝘴 𝘈𝘭𝘮𝘦𝘪𝘥𝘢 𝘙𝘪𝘨𝘶𝘦𝘪𝘳𝘢 <117239590+matheusalrg@users.noreply.github.com>
2026-03-18 23:18:54 -0300

    Update tarefa05.c

--- tarefas/tarefa05/tarefa05.c
+++ tarefas/tarefa05/tarefa05.c
@@ -1,1 +1,28 @@
+# include <stdio.h>
+# include <locale.h>
 
+int main() {
+    setlocale(LC_ALL, "Portuguese");
+    // Variável pontuação
+    int pontuacao = 0;
+
+    // Ponteiro
+    int *ponteiro_pontuacao;
+
+    // Conectando o ponteiro ao endereço de memória da variável pontuação
+    ponteiro_pontuacao = &pontuacao;
+
+    // Mostra na tela a pontuação inicial do jogador
+    printf("Pontuacao inicial: %d\n", pontuacao);
+
+    // Laço de repetição que representa as 3 moedas cotetadas pelo jogador
+    for (int i = 0; i < 3; i++) {
+        *ponteiro_pontuacao = *ponteiro_pontuacao + 10;
+    }
+
+    // Mostra na tela a pontuação final do jogador
+    printf("Pontuacao final apos coletar 3 moedas: %d\n", pontuacao);
+    
+
+    return 0;
+}
