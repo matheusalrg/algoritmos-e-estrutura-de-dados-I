cac1a48747eab9d186e19af332e46999b33aab99
𝘔𝘢𝘵𝘩𝘦𝘶𝘴 𝘈𝘭𝘮𝘦𝘪𝘥𝘢 𝘙𝘪𝘨𝘶𝘦𝘪𝘳𝘢 <117239590+matheusalrg@users.noreply.github.com>
2026-03-25 19:07:49 -0300

    Update tarefa06.c

--- tarefas/tarefa06/tarefa06.c
+++ tarefas/tarefa06/tarefa06.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <locale.h>
 
+int main() {
+    setlocale(LC_ALL, "Portuguese");
+    // Declaração de um vetor com 5 valores distintos
+    int plataformas[5] = {10, 20, 5, 15, 30};
+
+    // Criação de um ponteiro do tipo inteiro
+    int *ponteiro_plataforma;
+
+    // Variável do tipo inteiro chamada altura_total
+    int altura_total = 0;
+
+    // Laço que percorre todas as posições do vetor
+    for (int i = 0; i < 5; i++) {
+        // Faz o ponteiro apontar para o i-ésimo elemento do vetor
+        ponteiro_plataforma = plataformas + i;
+
+        // Soma o valor apontado à altura total
+        altura_total = altura_total + *ponteiro_plataforma;
+    }
+    // Mensagem final que será mostrada na tela do usuário
+    printf("Altura total do salto: %d", altura_total);
+
+    return 0;
+}
