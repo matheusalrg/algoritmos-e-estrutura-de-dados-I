7fbf47696485b007ef2015e14855fe9a50d0122b
𝘔𝘢𝘵𝘩𝘦𝘶𝘴 𝘈𝘭𝘮𝘦𝘪𝘥𝘢 𝘙𝘪𝘨𝘶𝘦𝘪𝘳𝘢 <117239590+matheusalrg@users.noreply.github.com>
2026-04-10 01:14:52 -0300

    Update tarefa15.c

--- tarefas/tarefa15/tarefa15.c
+++ tarefas/tarefa15/tarefa15.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#include <locale.h>
+#include <string.h>
 
+int main () {
+    setlocale(LC_ALL, "Portuguese");
+    // Declaração das variáveis para armazenar nome e título do herói
+    char nome_jogador[50];
+    char titulo_conquistado[50];
+
+    // Lê o nome do herói
+    printf("Digite o nome do heroi: \n");
+    scanf(" %[^\n]", nome_jogador); // lê até apertar a  tecla enter
+
+    // Lê o título conquistado com os espaçamentos
+    printf("Digite o titulo conquistado: \n");
+    scanf(" %[^\n]", titulo_conquistado); 
+
+    // Adiciona um espaço ao final do nome
+    strcat(nome_jogador, " ");
+
+    // Junta o título ao nome
+    strcat(nome_jogador, titulo_conquistado);
+
+    // Mostra o resultado final
+    printf("O heroi agora e conhecido como: %s\n", nome_jogador);
+
+    return 0;
+}
