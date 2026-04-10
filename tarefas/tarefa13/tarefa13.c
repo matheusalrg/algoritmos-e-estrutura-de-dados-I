3e61198de0eb3bd31f505014a777cc380bae5b33
𝘔𝘢𝘵𝘩𝘦𝘶𝘴 𝘈𝘭𝘮𝘦𝘪𝘥𝘢 𝘙𝘪𝘨𝘶𝘦𝘪𝘳𝘢 <117239590+matheusalrg@users.noreply.github.com>
2026-04-10 00:30:31 -0300

    Update tarefa13.c

--- tarefas/tarefa13/tarefa13.c
+++ tarefas/tarefa13/tarefa13.c
@@ -1,1 +1,26 @@
+#include  <stdio.h>
+#include <locale.h>
+#include <string.h>
 
+int main() {
+    setlocale(LC_ALL, "Portuguese");
+    // Variável palavra secreta
+    char palavra_secreta[] = "LUX";
+    // Tentativa do jogador (até 50 caracteres)
+    char tentativa_jogador[50];
+
+    // Mensagem para o jogador
+    printf("Uma voz ancestral ecoa... 'Diga a palavra e passe':\n");
+
+    // Ler a palavra digitada
+    scanf("%s", tentativa_jogador);
+
+    // Comparar as palavras
+    if (strcmp(palavra_secreta, tentativa_jogador) == 0) {
+        printf("A porta magica se abre!\n");
+    } else {
+        printf("Nada acontece... Parece que a palavra esta incorreta.\n");
+    }
+
+    return 0;
+}
