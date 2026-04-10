b9f11b6c0e5f0dc19ae075fd2987eb946650b977
𝘔𝘢𝘵𝘩𝘦𝘶𝘴 𝘈𝘭𝘮𝘦𝘪𝘥𝘢 𝘙𝘪𝘨𝘶𝘦𝘪𝘳𝘢 <117239590+matheusalrg@users.noreply.github.com>
2026-04-10 00:52:20 -0300

    Update tarefa14.c

--- tarefas/tarefa14/tarefa14.c
+++ tarefas/tarefa14/tarefa14.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <locale.h>
+#include <string.h>
 
+int main () {
+    setlocale(LC_ALL, "Portuguese");
+    // Frase padrão do chefe
+    char frase_inicio_luta[] = "Você nunca sairá daqui com vida!";
+
+    // Caixa de diálogo (inicialmente em silêncio)
+    char caixa_dialogo[100] = "(Silêncio)";
+
+    // Estado inicial do chefe
+    printf("Chefe antes da luta: %s\n", caixa_dialogo);
+
+    // Copiar a frase para a caixa de diálogo
+    strcpy(caixa_dialogo, frase_inicio_luta);
+
+    // Mostra o diálogo para o usuário após a ativação
+    printf("Chefe diz: %s\n", caixa_dialogo);
+
+    return 0;
+}
