739a36c4ee59a1d88a7506690853920028cd7284
𝘔𝘢𝘵𝘩𝘦𝘶𝘴 𝘈𝘭𝘮𝘦𝘪𝘥𝘢 𝘙𝘪𝘨𝘶𝘦𝘪𝘳𝘢 <117239590+matheusalrg@users.noreply.github.com>
2026-04-16 20:53:13 -0300

    Update tarefa17.c

--- tarefas/tarefa17/tarefa17.c
+++ tarefas/tarefa17/tarefa17.c
@@ -2,7 +2,7 @@
 #include <locale.h>
 
 int main() {
-    setlocale(LC_ALL, "pt_BR.UTF-8");
+    setlocale(LC_ALL, "Portuguese");
 
     // Definindo e inicializando o mapa
     int mapa_fase1[5][8] = {
