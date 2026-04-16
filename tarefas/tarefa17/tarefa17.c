c28eb92519f2aae32b584d3197c8b212ae021834
𝘔𝘢𝘵𝘩𝘦𝘶𝘴 𝘈𝘭𝘮𝘦𝘪𝘥𝘢 𝘙𝘪𝘨𝘶𝘦𝘪𝘳𝘢 <117239590+matheusalrg@users.noreply.github.com>
2026-04-16 20:15:54 -0300

    Update tarefa17.c

--- tarefas/tarefa17/tarefa17.c
+++ tarefas/tarefa17/tarefa17.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <locale.h>
 
+int main() {
+    setlocale(LC_ALL, "pt_BR.UTF-8");
+
+    // Definindo e inicializando o mapa
+    int mapa_fase1[5][8] = {
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {1, 1, 0, 0, 2, 0, 1, 1},
+        {0, 0, 1, 1, 0, 1, 0, 0},
+        {1, 1, 1, 1, 2, 2, 1, 1}
+    };
+
+    // Percorrendo a matriz
+    for (int i = 0; i < 5; i++) {          // número de linhas
+        for (int j = 0; j < 8; j++) {      // número de colunas
+            if (mapa_fase1[i][j] == 2) {
+                // Mensagem para o usuário
+                printf("Espinho encontrado na linha %d, coluna %d\n", i, j);
+            }
+        }
+    }
+
+    return 0;
+}
