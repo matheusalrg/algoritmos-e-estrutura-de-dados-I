c7e7b3315c02da8a192f373a927b7a2446758345
𝘔𝘢𝘵𝘩𝘦𝘶𝘴 𝘈𝘭𝘮𝘦𝘪𝘥𝘢 𝘙𝘪𝘨𝘶𝘦𝘪𝘳𝘢 <117239590+matheusalrg@users.noreply.github.com>
2026-04-16 20:59:12 -0300

    Update tarefa21.c

--- tarefas/tarefa21/tarefa21.c
+++ tarefas/tarefa21/tarefa21.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
 
+int main() {
+    setlocale(LC_ALL, "Portuguese");
+
+    // Ponteiro para armazenar a duração da fumaça
+    int *duracao_fumaca;
+
+    // Alocação de memória (a bomba explode!)
+    duracao_fumaca = (int *) malloc(sizeof(int));
+
+    // Verificando se a alocação foi bem-sucedida
+    if (duracao_fumaca == NULL) {
+        printf("Erro ao alocar memória para a fumaça.\n");
+        return 1;
+    }
+
+    // Definindo a duração da fumaça
+    *duracao_fumaca = 5;
+
+    // Imprimindo a ativação
+    printf("Bomba de fumaca ativada! Duracao: %d segundos.\n", *duracao_fumaca);
+
+    // Liberando a memória (a fumaça se dissipa)
+    free(duracao_fumaca);
+
+    // Boa prática: evitando ponteiro pendurado
+    duracao_fumaca = NULL;
+
+    // Imprimindo a finalização
+    printf("A fumaca se dissipou.\n");
+
+    return 0;
+}
