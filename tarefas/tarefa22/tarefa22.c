31b7ba1893008e515b6e8f2f5d8de64de570bb56
𝘔𝘢𝘵𝘩𝘦𝘶𝘴 𝘈𝘭𝘮𝘦𝘪𝘥𝘢 𝘙𝘪𝘨𝘶𝘦𝘪𝘳𝘢 <117239590+matheusalrg@users.noreply.github.com>
2026-04-16 21:06:22 -0300

    Update tarefa22.c

--- tarefas/tarefa22/tarefa22.c
+++ tarefas/tarefa22/tarefa22.c
@@ -1,1 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
 
+int main() {
+    setlocale(LC_ALL, "Portuguese");
+
+    // Variável para armazenar a quantidade de inimigos
+    int n_inimigos;
+
+    // Ponteiro para armazenar a onda de inimigos
+    int *ponteiro_onda;
+
+    // Entrada do usuário
+    printf("Digite a quantidade de inimigos da onda: ");
+    scanf("%d", &n_inimigos);
+
+    // Alocação dinâmica de memória para os inimigos
+    ponteiro_onda = (int *) malloc(n_inimigos * sizeof(int));
+
+    // Verificando se a alocação foi bem-sucedida
+    if (ponteiro_onda == NULL) {
+        printf("Erro ao alocar memoria para a onda de inimigos.\n");
+        return 1;
+    }
+
+    // Populando a onda com os IDs dos inimigos
+    for (int i = 0; i < n_inimigos; i++) {
+        printf("Digite o ID do inimigo %d: ", i + 1);
+        scanf("%d", &ponteiro_onda[i]);
+    }
+
+    // Listando os inimigos criados
+    printf("Onda de inimigos criada com sucesso! IDs: ");
+    for (int i = 0; i < n_inimigos; i++) {
+        printf("%d ", ponteiro_onda[i]);
+    }
+    printf("\n");
+
+    // Liberando a memória alocada
+    free(ponteiro_onda);
+
+    // Boa prática: evitando ponteiro pendurado
+    ponteiro_onda = NULL;
+
+    return 0;
+}
