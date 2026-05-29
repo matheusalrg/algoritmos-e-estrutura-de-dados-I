995c092bec3580fd359264d49807730b2884b05f
𝘔𝘢𝘵𝘩𝘦𝘶𝘴 𝘈𝘭𝘮𝘦𝘪𝘥𝘢 𝘙𝘪𝘨𝘶𝘦𝘪𝘳𝘢 <117239590+matheusalrg@users.noreply.github.com>
2026-05-29 00:35:40 -0300

    Update tarefa49.c

--- tarefas/tarefa49/tarefa49.c
+++ tarefas/tarefa49/tarefa49.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
 
+int main() {
+    setlocale(LC_ALL, "Portuguese");
+
+    // Ponteiro para o arquivo de mapa
+    FILE *arquivo;
+
+    // Tentando abrir o arquivo de mapa
+    arquivo = fopen("level_1_map.dat", "r");
+
+    // Verificando se houve erro na abertura do arquivo
+    if (arquivo == NULL) {
+
+        // Mensagem de erro personalizada
+        printf("ERRO CRITICO: Nao foi possivel carregar os dados do mapa.\n");
+
+        // Exibindo detalhes do erro do sistema operacional
+        perror("Detalhes do erro");
+
+        // Encerrando o programa com código de erro
+        exit(1);
+    }
+
+    // Se o arquivo abrir corretamente (caso exista no futuro)
+    fclose(arquivo);
+
+    return 0;
+}
