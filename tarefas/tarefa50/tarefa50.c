5b7e168a6a13235d26859412b2dc718534e6baad
𝘔𝘢𝘵𝘩𝘦𝘶𝘴 𝘈𝘭𝘮𝘦𝘪𝘥𝘢 𝘙𝘪𝘨𝘶𝘦𝘪𝘳𝘢 <117239590+matheusalrg@users.noreply.github.com>
2026-05-29 00:41:05 -0300

    Update tarefa50.c

--- tarefas/tarefa50/tarefa50.c
+++ tarefas/tarefa50/tarefa50.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+#include <locale.h>
 
+int main() {
+    setlocale(LC_ALL, "Portuguese");
+
+    // Variáveis para armazenar os valores de entrada
+    float forcaBase, bonusArma, bonusBuff;
+
+    // Variável para armazenar o dano total
+    float danoTotal;
+
+    // Lendo a força base do personagem
+    printf("Digite a força base do personagem: ");
+    scanf("%f", &forcaBase);
+
+    // Lendo o bônus de dano da arma
+    printf("Digite o bônus de dano da arma: ");
+    scanf("%f", &bonusArma);
+
+    // Lendo o bônus do feitiço (buff)
+    printf("Digite o bônus do feitiço: ");
+    scanf("%f", &bonusBuff);
+
+    // Calculando o dano total (tempo constante O(1))
+    danoTotal = forcaBase + bonusArma + bonusBuff;
+
+    // Exibindo o resultado com duas casas decimais
+    printf("Dano total: %.2f\n", danoTotal);
+
+    return 0;
+}
