86dc7da829be20bc7339fc1bc61c2000300276e6
𝘔𝘢𝘵𝘩𝘦𝘶𝘴 𝘈𝘭𝘮𝘦𝘪𝘥𝘢 𝘙𝘪𝘨𝘶𝘦𝘪𝘳𝘢 <117239590+matheusalrg@users.noreply.github.com>
2026-05-28 23:03:47 -0300

    Update tarefa41.c

--- tarefas/tarefa41/tarefa41.c
+++ tarefas/tarefa41/tarefa41.c
@@ -1,1 +1,70 @@
+#include <stdio.h>
+#include <locale.h>
 
+int main() {
+    setlocale(LC_ALL, "Portuguese");
+
+    // Variável da opção do menu
+    int opcao;
+
+    // Vetor para armazenar o evento
+    char evento[100];
+
+    // Ponteiro para o arquivo
+    FILE *arquivo;
+
+    // Laço principal do programa
+    do {
+
+        // Exibindo menu
+        printf("\n=== SISTEMA DE EVENTOS ===\n");
+        printf("1 - Registrar novo evento\n");
+        printf("2 - Sair\n");
+        printf("Escolha uma opcao: ");
+        scanf("%d", &opcao);
+
+        // Verificando opção escolhida
+        if (opcao == 1) {
+
+            // Abrindo arquivo no modo append
+            arquivo = fopen("game_log.txt", "a");
+
+            // Verificando se o arquivo abriu corretamente
+            if (arquivo == NULL) {
+
+                printf("Erro ao abrir o arquivo!\n");
+                return 1;
+            }
+
+            // Lendo descrição do evento
+            printf("\nDigite o evento: ");
+            scanf("%s", evento);
+
+            // Salvando evento no arquivo
+            fprintf(arquivo, "%s\n", evento);
+
+            // Forçando escrita imediata no arquivo
+            fflush(arquivo);
+
+            // Fechando o arquivo
+            fclose(arquivo);
+
+            printf("Evento registrado com sucesso!\n");
+        }
+
+        // Verificando opção de saída
+        else if (opcao == 2) {
+
+            printf("\nEncerrando programa...\n");
+        }
+
+        // Tratando opção inválida
+        else {
+
+            printf("\nOpcao invalida!\n");
+        }
+
+    } while (opcao != 2);
+
+    return 0;
+}
