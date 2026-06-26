d9caef115c43b6c07097cf2653512eb8954fadd5
𝘔𝘢𝘵𝘩𝘦𝘶𝘴 𝘈𝘭𝘮𝘦𝘪𝘥𝘢 𝘙𝘪𝘨𝘶𝘦𝘪𝘳𝘢 <117239590+matheusalrg@users.noreply.github.com>
2026-06-26 01:22:09 -0300

    Update tarefa79.c

--- tarefas/tarefa79/tarefa79.c
+++ tarefas/tarefa79/tarefa79.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "heroi/heroi.h"
 
+int main()
+{
+    int quantidade;
+    Heroi *herois;
+
+    printf("Quantidade de herois: ");
+    scanf("%d", &quantidade);
+
+    herois = criarHerois(quantidade);
+
+    if (herois == NULL)
+    {
+        printf("Erro ao alocar memoria.\n");
+        return 1;
+    }
+
+    lerHerois(herois, quantidade);
+
+    imprimirHerois(herois, quantidade);
+
+    free(herois);
+
+    return 0;
+}
